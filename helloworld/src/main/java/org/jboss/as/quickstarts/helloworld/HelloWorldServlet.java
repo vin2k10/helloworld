8d40d9eb9547d1296d15ce0b820365b880f6c9ae
Vinod Pai <vin2k10@hotmail.com>
2013-06-29 12:01:03 -0400

    Change 7 (work)

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -55,7 +55,7 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage4("World  - 6") + "</h1>");
+        writer.println("<h1>" + helloService.createHelloMessage4("World  - 7") + "</h1>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
